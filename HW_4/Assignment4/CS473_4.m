07f11c1ad97d6dc5975b6d9a96defa9e53a68c48
Evan <91156071+C-ouch@users.noreply.github.com>
2023-04-04 16:03:20 -0400

    parts  1 and 1.1

--- HW_4/Assignment4/CS473_4.m
+++ HW_4/Assignment4/CS473_4.m
@@ -1,6 +1,21 @@
 load dalekosaur/object.mat
 
-patch('vertices', Xo’, 'faces', Faces, 'facecolor', 'w', 'edgecolor', 'k');
+patch('vertices', Xo', 'faces', Faces, 'facecolor', 'w', 'edgecolor', 'k');
 axis vis3d;
 axis equal;
 xlabel('Xo-axis'); ylabel('Yo-axis'); zlabel('Zo-axis');
+ObjectDirectory = 'dalekosaur';
+InputImage = 'image1.jpg';
+
+[impoints, objpoints3D] = clickPoints( InputImage, ObjectDirectory );
+
+figure;
+imshow(InputImage); hold on;
+plot( impoints2D(:,1), impoints2D(:,2), 'b.');
+%and the object points by calling
+figure;
+patch('vertices', Xo', 'faces', Faces, 'facecolor', 'w', 'edgecolor', 'k');
+axis vis3d;
+axis equal;
+plot3( objpoints3D(:,1), objpoints3D(:,2), objpoints3D(:,3), 'b.' );
+
