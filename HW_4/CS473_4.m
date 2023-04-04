a171df2110487c89ea9cbdd4ee18686648daaf70
Caerii <cubelocked@gmail.com>
2023-04-04 15:22:05 -0400

    matlab files

---
+++ HW_4/CS473_4.m
@@ -0,0 +1,6 @@
+load dalekosaur/object.mat
+
+patch('vertices', Xo’, 'faces', Faces, 'facecolor', 'w', 'edgecolor', 'k');
+axis vis3d;
+axis equal;
+xlabel('Xo-axis'); ylabel('Yo-axis'); zlabel('Zo-axis');
